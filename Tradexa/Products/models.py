b88e5a455557185fd94f5f8925c91dbaa0e29d2f
arishta <18ucs002@lnmiit.ac.in>
2021-07-22 18:25:52 +0530

    Added models

--- Tradexa/Products/models.py
+++ Tradexa/Products/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
-# Create your models here.
+class Product(models.Model):
+    name = models.CharField(max_length = 255)
+    weight = models.DecimalField(max_digits = 4, decimal_places = 2)
+    created_at = models.DateTimeField(blank = True, null = True, auto_now_add = True)
+    updated_at = models.DateTimeField(auto_now = True)
+    price = models.DecimalField(max_digits = 6, decimal_places = 2)
+
+    def __str__(self):
+        return self.name
+
+    class Meta: 
+        db_table = "product"
--- Tradexa/User/models.py
+++ Tradexa/User/models.py
@@ -1,3 +1,25 @@
 from django.db import models
 
-# Create your models here.
+class User(models.Model):
+    username   = models.CharField(max_length = 50,unique = True)
+    first_name  = models.CharField(max_length = 50)
+    last_name   = models.CharField(max_length = 50)
+    email  = models.EmailField(max_length = 70,blank = True,unique = True)
+    password = models.CharField(max_length = 20)
+
+    def __str__(self):
+        return "%s %s" % (self.first_name, self.last_name)
+    class Meta:
+      	db_table = "user"
+
+class Post(models.Model):
+ 	user = models.ForeignKey(User, on_delete = models.CASCADE)
+ 	title = models.CharField(max_length = 100)
+ 	text = models.TextField()
+ 	created_at = models.DateTimeField(blank = True, null = True, auto_now_add = True)
+ 	updated_at = models.DateTimeField(auto_now = True)
+ 	def __str__(self):
+ 		return self.title
+
+ 	class Meta:
+ 		db_table = "post"
